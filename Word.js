d4f4b1dbceb3301a5718c18dd7b2f76e936fafcc
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-05 11:46:28 -0600

    Word works after changing naming jargon

--- Word.js
+++ Word.js
@@ -1,10 +1,11 @@
 var Letter = require("./Letter.js");
 
-// var A = new Letter(`a`, false);
-// A.checkLetter("B");
+var A = new Letter(`a`, false);
+A.checkLetter("b");
+console.log(A.placeholder());
 
 function Word(word) {
-  this.CreateletterObj = function() {
+  this.CreateletterObjArr = function() {
     var wordArray = word.split("");
     var letterObjArr = [];
     for (var i = 0; i < wordArray.length; i++) {
@@ -12,23 +13,25 @@ function Word(word) {
     }
     return letterObjArr;
   };
+  this.letterObjArray = this.CreateletterObjArr();
   this.displayWord = function() {
     var dashedWord = "";
-    for (var i = 0; i < this.CreateletterObj().length; i++) {
-      dashedWord += this.CreateletterObj()[i].placeholder();
+    for (var i = 0; i < this.letterObjArray.length; i++) {
+      dashedWord += this.letterObjArray[i].placeholder();
     }
     console.log(dashedWord);
   };
   this.checkGuess = function(guess) {
-    for (var i = 0; i < this.CreateletterObj().length; i++) {
-      this.CreateletterObj()[i].checkLetter(guess);
-      console.log(this.CreateletterObj()[i].isGuessed);
+    for (var i = 0; i < this.letterObjArray.length; i++) {
+      this.letterObjArray[i].checkLetter(guess);
     }
     this.displayWord();
   };
 }
 
-var foo = new Word("foo");
-
+var foo = new Word("Funny");
+console.log(foo.CreateletterObjArr());
 foo.displayWord();
-foo.checkGuess("F");
+foo.checkGuess("N");
+
+module.exports = Word;
