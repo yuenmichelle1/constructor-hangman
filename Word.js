6f1d5758466fc54c7f30fe14a3d564227f8e7efe
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-05 16:25:21 -0600

    doing maps

--- Word.js
+++ Word.js
@@ -3,15 +3,12 @@ var Letter = require("./Letter.js");
 function Word(word) {
   this.CreateletterObjArr = function() {
     var wordArray = word.split("");
-    var letterObjArr = wordArray.map((letter) => new Letter (letter, false));
+    var letterObjArr = wordArray.map(letter => new Letter(letter, false));
     return letterObjArr;
   };
   this.letterObjArray = this.CreateletterObjArr();
   this.displayWord = function() {
-    var dashedWord = "";
-    for (var i = 0; i < this.letterObjArray.length; i++) {
-      dashedWord += this.letterObjArray[i].placeholder();
-    }
+    var dashedWord = this.letterObjArray.map(letterObj => letterObj.placeholder()).join(" ");
     console.log(dashedWord);
     return dashedWord;
   };
@@ -24,8 +21,8 @@ function Word(word) {
 }
 
 var foo = new Word("Monkey");
-// foo.displayWord();
-// foo.checkGuess("u");
-console.log(foo.CreateletterObjArr());
+foo.displayWord();
+// // foo.checkGuess("u");
+// console.log(foo.CreateletterObjArr());
 
 module.exports = Word;
