f884dbaa4d3fa6f03c5ed6d1100a57daf1cc177a
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-07 01:22:28 -0600

    checking if wrong

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Word = require("./Word.js");
 var inquirer = require("inquirer");
 var alphabet = [`A`,`B`,`C`, `D`,`E`,`F`,`G`, `H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`, `S`,`T`,`U`, `V`, `W`,`X`,`Y`,`Z`];
-
-
+var userGuess;
+//add an empty array letters used later
 var game = {
   wordBank: [
     `Thor`,
@@ -23,15 +23,23 @@ var game = {
   losses: 0,
   numberOfguesses: 8,
   wrongGuesses: [],
+  dashesOrletters: "",
   startGame: function() {
     //ask user to start the game, then start if yes
+    this.resetSettings();
     this.currentWordObj = new Word(this.wordBank[Math.floor(Math.random()* this.wordBank.length)]);
     AskThenStart(this.currentWordObj);
+  },
+  resetSettings: function(){
+    this.numberOfguesses = 8;
+    this.wrongGuesses =[];
+    this.dashesOrletters = "";
   }
 };
 
 function InquireLetter(currentWordObj) {
-  if (game.numberOfguesses > 0 && currentWordObj.displayWord().includes(`_`)) {
+  game.dashesOrletters = currentWordObj.displayWord();
+  if (game.numberOfguesses > 0 && game.dashesOrletters.includes(`_`)) {
     inquirer
       .prompt([
         {
@@ -41,13 +49,14 @@ function InquireLetter(currentWordObj) {
         }
       ])
       .then(function(inquirerResponse) {
-        var userGuess= inquirerResponse.userGuess;
-        console.log(`you guessed ${userGuess}`);
+        userGuess= inquirerResponse.userGuess;
+        console.log(`Correct! you guessed ${userGuess}`);
         if (
           userGuess.length === 1 &&
           alphabet.includes(userGuess.toUpperCase())
         ) {
           currentWordObj.checkGuess(userGuess);
+          pushWrongGuess(currentWordObj);
         } else {
           console.log(
             `${userGuess} is not a valid input. Please guess a letter`
@@ -76,6 +85,20 @@ function AskThenStart(currentWordObj) {
       }
     });
 }
+
+function pushWrongGuess (currentWordObj) {
+  //if user guesses Wrong (i.e. isGuessed for every Letter Obj in array is false), then push userGuess
+  var wordLetterObjArr = currentWordObj.letterObjArray;
+  var arrayOfWordLetters = wordLetterObjArr.map(Letter => Letter.letter);
+  
+  if (arrayOfWordLetters.indexOf(userGuess.toUpperCase()) === -1){
+    game.wrongGuesses.push(userGuess.toUpperCase());
+    game.numberOfguesses--;
+    console.log(`Wrong! You have ${game.numberOfguesses} remaining`);
+  }
+  
+}
+
 
 
 game.startGame();
